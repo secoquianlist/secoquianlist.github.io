093e7b77d442084d5748bf6a74f9dee4f79bdd9a
JSecoquian <74154167+JSecoquian@users.noreply.github.com>
2020-11-08 15:51:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 window.onload = function()
   {
-    populateshoppingListsonload();
-      displayshoppingLists();
+    alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+    populateshoppinglistonload();
+    displayShoppinglists();
+    clearFocus();
   };
 
 function readCookie(name)
