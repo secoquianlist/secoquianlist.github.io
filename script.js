9c373b04b2da041d4a8bd92f9fa34632f7c06b92
JSecoquian <74154167+JSecoquian@users.noreply.github.com>
2020-12-09 21:06:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function passlist()
       };
 
     $.ajax({
-        url: "https://bit.ly/36X9YuF",
+        url: "https://api-ssl.bitly.com/v4/shorten",
         cache: false,
         dataType: "json",
         method: "POST",
@@ -56,12 +56,12 @@ function share()
 
 function copyToClipboard(text) 
   {
-    var passbyurl = document.createElement("textarea");
+    const passbyurl = document.createElement("textarea");
     passbyurl.value = text;
     document.body.appendChild(passbyurl);
     passbyurl.focus();
     passbyurl.select();
-    document.execCommand("copy");
+    document.execCommand('copy');
     document.body.removeChild(passbyurl);
     alert("Added to your clipboard and ready to share!" + text);
   }
